b77ed5529666fc5a0f7fa02c231d24be9b1f5d74
Joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-04 13:56:42 +0000

    Fix erro de sintaxe no WezTerm

--- modules/theme.nix
+++ modules/theme.nix
@@ -23,31 +23,6 @@
     };
   };
 
-  # Wezterm configuration with Catppuccin
-  programs.wezterm = {
-    enable = true;
-    extraConfig = ''
-      local wezterm = require 'wezterm'
-      return {
-        color_scheme = 'Catppuccin Mocha',
-        font = wezterm.font('JetBrainsMono Nerd Font'),
-        font_size = 12.0,
-        hide_tab_bar_if_only_one_tab = true,
-        window_background_opacity = 0.9,
-        enable_wayland = true,
-        window_close_confirmation = 'NeverPrompt',
-        default_prog = { 'zsh' },
-      }
-    '';
-  };
-
-  # DankMaterialShell (DMS) dynamic theming
-  programs.dank-material-shell = {
-    enableDynamicTheming = true;
-    # DMS uses matugen to generate colors from wallpaper,
-    # but we can hint it towards catppuccin colors if needed.
-  };
-
   # Force cursor and icons via dconf for Wayland/Niri
   dconf.settings = {
     "org/gnome/desktop/interface" = {
