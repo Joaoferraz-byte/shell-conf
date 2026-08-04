aaa870c09540af16cb06615f363a9721da0847b2
Joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-04 09:55:05 +0000

    fix: remove duplicate niri-flake import to resolve programs.niri option conflict

--- modules/default.nix
+++ modules/default.nix
@@ -3,13 +3,11 @@
 
 {
   imports = [
-    # niri-flake home-manager module
-    inputs.niri.homeModules.niri
-
     # DMS home module (provides programs.dank-material-shell options)
     inputs.dms.homeModules.dank-material-shell
 
     # DMS niri integration module (provides programs.dank-material-shell.niri options)
+    # This module internally imports niri-flake home-manager module.
     inputs.dms.homeModules.niri
 
     ./dms.nix
