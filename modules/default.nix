d4794c19fb48a059662476da7309cb4fe8a70d29
joaoferraz-byte <joaoferraz467@gmail.com>
2026-08-08 16:41:22 +0000

    feat(niri): import niri settings home module into DMS module

--- modules/default.nix
+++ modules/default.nix
@@ -6,6 +6,11 @@
     # DMS home module (provides programs.dank-material-shell options)
     inputs.dms.homeModules.dank-material-shell
 
+    # niri-flake Home Manager module (provides programs.niri settings options)
+    inputs.niri.homeModules.niri
+    # Niri settings: keybinds (incl. DMS wallpaper IPC), environment, layout
+    ./niri.nix
+
 
     ./dms.nix
     ./theme.nix
